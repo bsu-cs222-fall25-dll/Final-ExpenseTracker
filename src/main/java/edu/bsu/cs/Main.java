ec9e5eb274071f93d165919e0ec4dfb045192a48
Nota30 <harenkeshaav11@gmail.com>
2025-10-16 15:35:21 -0400

    Add partial user interface and options

--- src/main/java/edu/bsu/cs/Main.java
+++ src/main/java/edu/bsu/cs/Main.java
@@ -1,4 +1,22 @@
 package edu.bsu.cs;
 
 public class Main {
+    public static void main(String[] args) {
+        Output output = new Output();
+        Input input = new Input();
+        Expense expense = new Expense();
+        Income income = new Income();
+
+        output.showMenu();
+
+        int choice = input.getChoice();
+        int amount;
+        String description;
+        if  (choice == 1) {
+            amount = input.getAmount();
+            description = input.getDescription();
+            expense.addTransaction(amount, description);
+        } else  if  (choice == 2) {
+        }
+    }
 }
