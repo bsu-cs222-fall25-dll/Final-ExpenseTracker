935de0e9d4992c5bc38de4d6d666b27f4c0838b6
Nota30 <harenkeshaav11@gmail.com>
2025-10-22 14:11:04 -0400

    Refactor test transaction to use types

--- src/test/java/edu/bsu/cs/TransactionTest.java
+++ src/test/java/edu/bsu/cs/TransactionTest.java
@@ -7,14 +7,14 @@ public class TransactionTest {
 
     @Test
     public void testGetAmount() {
-        Transaction transaction = new Transaction(200, "Spent on ice cream");
+        Transaction transaction = new Transaction(200, true, "Spent on ice cream");
         int amount = transaction.getAmount();
         Assertions.assertEquals(200, amount);
     }
 
     @Test
     public void testGetDescription() {
-        Transaction transaction = new Transaction(200, "Spent on ice cream");
+        Transaction transaction = new Transaction(200, false, "Spent on ice cream");
         String description = transaction.getDescription();
         Assertions.assertEquals("Spent on ice cream", description);
     }
