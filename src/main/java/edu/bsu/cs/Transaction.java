2f9cbc08de4a01548c9272a831bcb4bf7f03ff6d
Nota30 <harenkeshaav11@gmail.com>
2025-10-22 14:11:35 -0400

    Refactor transaction to add types

--- src/main/java/edu/bsu/cs/Transaction.java
+++ src/main/java/edu/bsu/cs/Transaction.java
@@ -3,14 +3,21 @@ package edu.bsu.cs;
 public class Transaction {
     private int amount;
     private String description;
+    // True = expense, False = income
+    private final boolean type;
 
-    public Transaction(int amount, String description) {
+    public Transaction(int amount, boolean type, String description) {
         this.amount = amount;
+        this.type = type;
         this.description = description;
     }
 
     public int getAmount() {
         return amount;
+    }
+
+    public boolean getType() {
+        return type;
     }
 
     public void setAmount(int amount) {
