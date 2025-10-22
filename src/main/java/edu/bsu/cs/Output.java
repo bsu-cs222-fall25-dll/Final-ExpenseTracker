1e4df3035c0cce653cb7d41e683036b1bc3e8cf4
Nota30 <harenkeshaav11@gmail.com>
2025-10-22 16:40:47 -0400

    Refactor output class with proper output design

--- src/main/java/edu/bsu/cs/Output.java
+++ src/main/java/edu/bsu/cs/Output.java
@@ -1,39 +1,65 @@
 package edu.bsu.cs;
 
+import java.util.ArrayList;
+
 public class Output {
     public void showMenu() {
-        System.out.println("Total : ");
-        System.out.println("Expense : ");
-        System.out.println("Income : ");
-        System.out.println("---------------- Menu ----------------");
-        System.out.println("1. Edit Expense");
-        System.out.println("2. Edit Income");
-        System.out.println("3. View All Transactions");
+        System.out.println("""
+                ---------------- Menu ----------------
+                1. Expense
+                2. Income
+                3. Remove Transaction
+                4. View All Transactions
+                5. Exit
+                """);
     }
-     public void showExpenseMenu(){
-        System.out.println("---------------- Expense ----------------");
-        showSecondaryOptions();
-     }
-     public void showIncomeMenu(){
-        System.out.println("-------------- Income ---------------");
-        showSecondaryOptions();
-     }
 
-     public void showSecondaryOptions() {
-         System.out.println("1. Add Transaction");
-         System.out.println("2. Remove Transaction");
-         System.out.println("3. View Transactions");
-     }
+    public void showExpenseMenu() {
+        System.out.println("""
+                ---------------- Expense Menu ----------------
+                1. Add Expense
+                2. View Expenses
+                """);
+    }
 
-     public void showTransaction(){
-        System.out.println("-------------- Transactions ----------------");
-     }
+    public void showIncomeMenu() {
+        System.out.println("""
+                ---------------- Income Menu ----------------
+                1. Add Income
+                2. View Incomes
+                """);
+    }
 
-      public void userAmountRequest(){
-        System.out.println("Enter amount : ");
-      }
+    public void showEnterAmount() {
+        System.out.print("Enter amount: ");
+    }
 
-      public void userDescriptionRequest(){
-        System.out.println("Enter description : ");
-      }
+    public void showEnterDescription() {
+        System.out.print("Enter description: ");
+    }
+
+    public void showRemoveTransaction() {
+        System.out.print("Enter Transaction ID to be removed: ");
+    }
+
+    public void showTransactions(ArrayList<Transaction> transactions) {
+        System.out.println("---------------- Transaction List ----------------");
+        for (int i = 0; i < transactions.size(); i++) {
+            Transaction transaction = transactions.get(i);
+            String type = "";
+            if (transaction.getType()) type = "Expense";
+            else type = "Income";
+
+            System.out.println((i + 1) + ". Amount: $" +
+                    transaction.getAmount() +
+                    " | Description: " +
+                    transaction.getDescription() +
+                    " | Type: " +
+                    type);
+        }
+    }
+
+    public void showEnterOption() {
+        System.out.print("Enter your option: ");
+    }
 }
