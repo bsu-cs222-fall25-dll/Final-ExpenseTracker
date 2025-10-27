d6664864010745ae05666b9e6cd79e54a42c6a0c
cynkou <chuysluis@gmail.com>
2025-10-27 10:00:23 -0400

    Refactor title and colored options for better color matching

--- src/main/java/edu/bsu/cs/Output.java
+++ src/main/java/edu/bsu/cs/Output.java
@@ -4,30 +4,51 @@ import java.util.ArrayList;
 
 public class Output {
     public static final String ANSI_RESET = "\u001b[0m";
-    public static final String ANSI_RED = "\u001b[31;1m";
-    public static final String ANSI_GREEN = "\u001b[32;1m";
+    public static final String ANSI_RED = "\u001b[31;4m";
+    public static final String ANSI_GREEN = "\u001b[92;1m";
     public static final String ANSI_BLUE = "\u001b[34;1m";
-    public static final String ANSI_YELLOW = "\u001b[33;1m";
+    public static final String ANSI_YELLOW = "\u001b[93;1m";
     public static final String ANSI_MAGENTA = "\u001b[35;1m";
-    public static final String ANSI_CYAN = "\u001b[36;1m";
+    public static final String ANSI_CYAN = "\u001B[4;96m";
 
     public void showMenu() {
         System.out.print("\n");
-        System.out.println(ANSI_GREEN + """ 
-                 _____                                  _____               _            \s
-                | ____|_  ___ __   ___ _ __  ___  ___  |_   _| __ __ _  ___| | _____ _ __\s
-                |  _| \\ \\/ / '_ \\ / _ \\ '_ \\/ __|/ _ \\   | || '__/ _` |/ __| |/ / _ \\ '__|
-                | |___ >  <| |_) |  __/ | | \\__ \\  __/   | || | | (_| | (__|   <  __/ |  \s
-                |_____/_/\\_\\ .__/ \\___|_| |_|___/\\___|   |_||_|  \\__,_|\\___|_|\\_\\___|_|  \s
-                           |_|                                                           \s
-                """ + ANSI_RESET);
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
 
-        System.out.println(ANSI_GREEN + " ---------------- Menu ----------------" + ANSI_RESET);
-        System.out.println(ANSI_CYAN + "1. Expense" + ANSI_RESET);
-        System.out.println(ANSI_MAGENTA + "2. Income" + ANSI_RESET);
-        System.out.println(ANSI_YELLOW + "3. Remove Transaction" + ANSI_RESET);
-        System.out.println(ANSI_BLUE + "4. View All Transactions" + ANSI_RESET);
-        System.out.println(ANSI_RED + "5. Exit" + ANSI_RESET);
+        System.out.println(ANSI_GREEN +"===============================================================");
+        System.out.println("|                                                             |");
+        System.out.println("|   " + ANSI_GREEN + "_____                                      "  + "               |");
+        System.out.println("|  "  + ANSI_GREEN + "| ____|_  ___ __   ___ _ __  ___  ___  ___ "  + "                |");
+        System.out.println("|  "  + ANSI_GREEN + "|  _| \\ \\/ / '_ \\ / _ \\ '_ \\/ __|/ _ \\/ __|"  + "                |");
+        System.out.println("|  "  + ANSI_GREEN + "| |___ >  <| |_) |  __/ | | \\__ \\  __/\\__ \\"  + "                |");
+        System.out.println("|  "  + ANSI_GREEN + "|_____/_/\\_\\ .__/ \\___|_| |_|___/\\___||___/"  + "                |");
+        System.out.println("|             " + ANSI_GREEN + "|_|"  + "                                             |");
+        System.out.println("|                                                             |");
+        System.out.println("|   " + ANSI_GREEN + "_______             _                 "  + "                    |");
+        System.out.println("|  "  + ANSI_GREEN + "|__   __|           | |                " + "                    |");
+        System.out.println("|     " + ANSI_GREEN + "| |_ __ __ _  ___| | _____ _ __     " + "                    |");
+        System.out.println("|     " + ANSI_GREEN + "| | '__/ _` |/ __| |/ / _ \\ '__|    " + "                    |");
+        System.out.println("|     " + ANSI_GREEN + "| | | | (_| | (__|   <  __/ |       " + "                    |");
+        System.out.println("|     " + ANSI_GREEN + "|_|_|  \\__,_|\\___|_|\\_\\___|_|       " + "                    |");
+        System.out.println("|                                                             |");
+        System.out.println("===============================================================" + ANSI_RESET );
+        System.out.println();
+
+        System.out.println(ANSI_GREEN + "---------------------------------------------------------------");
+        System.out.println("|                          MENU                               |");
+        System.out.println("---------------------------------------------------------------" + ANSI_RESET);
+        System.out.println("|                                                             |");
+        System.out.println("|   " + ANSI_CYAN + "1. Expense" + ANSI_RESET + "                                                |");
+        System.out.println("|   " + ANSI_CYAN + "2. Income" + ANSI_RESET + "                                                 |");
+        System.out.println("|   " + ANSI_CYAN + "3. Remove Transaction" + ANSI_RESET + "                                     |");
+        System.out.println("|   " + ANSI_CYAN + "4. View All Transactions" + ANSI_RESET + "                                  |");
+        System.out.println("|   " + ANSI_RED + "5. Exit" + ANSI_RESET + "                                                   |");
+        System.out.println(ANSI_GREEN + "|                                                             |");
+        System.out.println("---------------------------------------------------------------" + ANSI_RESET);
+
+
+
 
 
     }
