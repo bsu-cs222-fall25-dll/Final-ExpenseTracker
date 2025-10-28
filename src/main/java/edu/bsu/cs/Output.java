e28ab6b4cfb4b2cb1bc9e3af21415adf24c95a6d
Nota30 <harenkeshaav11@gmail.com>
2025-10-27 21:59:51 -0400

    Refactor output style to be cleaner

--- src/main/java/edu/bsu/cs/Output.java
+++ src/main/java/edu/bsu/cs/Output.java
@@ -10,83 +10,78 @@ public class Output {
     public static final String ANSI_YELLOW = "\u001b[93;1m";
     public static final String ANSI_MAGENTA = "\u001b[35;1m";
     public static final String ANSI_CYAN = "\u001B[4;96m";
+    public static final String ANSI_WHITE = "\u001B[37m";
 
     public void showMenu() {
-        System.out.print("\n");
+        System.out.println("\n");
         System.out.print("\033[H\033[2J");
         System.out.flush();
 
-        System.out.println(ANSI_GREEN +"===============================================================");
-        System.out.println("|                                                             |");
-        System.out.println("|   " + ANSI_GREEN + "_____                                      "  + "               |");
-        System.out.println("|  "  + ANSI_GREEN + "| ____|_  ___ __   ___ _ __  ___  ___  ___ "  + "                |");
-        System.out.println("|  "  + ANSI_GREEN + "|  _| \\ \\/ / '_ \\ / _ \\ '_ \\/ __|/ _ \\/ __|"  + "                |");
-        System.out.println("|  "  + ANSI_GREEN + "| |___ >  <| |_) |  __/ | | \\__ \\  __/\\__ \\"  + "                |");
-        System.out.println("|  "  + ANSI_GREEN + "|_____/_/\\_\\ .__/ \\___|_| |_|___/\\___||___/"  + "                |");
-        System.out.println("|             " + ANSI_GREEN + "|_|"  + "                                             |");
-        System.out.println("|                                                             |");
-        System.out.println("|   " + ANSI_GREEN + "_______             _                 "  + "                    |");
-        System.out.println("|  "  + ANSI_GREEN + "|__   __|           | |                " + "                    |");
-        System.out.println("|     " + ANSI_GREEN + "| |_ __ __ _  ___| | _____ _ __     " + "                    |");
-        System.out.println("|     " + ANSI_GREEN + "| | '__/ _` |/ __| |/ / _ \\ '__|    " + "                    |");
-        System.out.println("|     " + ANSI_GREEN + "| | | | (_| | (__|   <  __/ |       " + "                    |");
-        System.out.println("|     " + ANSI_GREEN + "|_|_|  \\__,_|\\___|_|\\_\\___|_|       " + "                    |");
-        System.out.println("|                                                             |");
-        System.out.println("===============================================================" + ANSI_RESET );
-        System.out.println();
-
-        System.out.println(ANSI_GREEN + "---------------------------------------------------------------");
-        System.out.println("|                          MENU                               |");
-        System.out.println("---------------------------------------------------------------" + ANSI_RESET);
-        System.out.println("|                                                             |");
-        System.out.println("|   " + ANSI_CYAN + "1. Expense" + ANSI_RESET + "                                                |");
-        System.out.println("|   " + ANSI_CYAN + "2. Income" + ANSI_RESET + "                                                 |");
-        System.out.println("|   " + ANSI_CYAN + "3. Remove Transaction" + ANSI_RESET + "                                     |");
-        System.out.println("|   " + ANSI_CYAN + "4. View All Transactions" + ANSI_RESET + "                                  |");
-        System.out.println("|   " + ANSI_RED + "5. Exit" + ANSI_RESET + "                                                   |");
-        System.out.println(ANSI_GREEN + "|                                                             |");
-        System.out.println("---------------------------------------------------------------" + ANSI_RESET);
-
-
-
-
-
+        System.out.println(ANSI_BLUE + """
+                ===============================================================
+                ||                         MENU                              ||
+                ===============================================================""");
+        System.out.println("||  " + ANSI_GREEN + "1. Expense Menu" + ANSI_RESET + "                                          " + ANSI_BLUE + "||");
+        System.out.println("||  " + ANSI_GREEN + "2. Income Menu" + ANSI_RESET + "                                           " + ANSI_BLUE + "||");
+        System.out.println("||  " + ANSI_GREEN + "3. Remove Transaction" + ANSI_RESET + "                                    " + ANSI_BLUE + "||");
+        System.out.println("||  " + ANSI_GREEN + "4. View All Transactions" + ANSI_RESET + "                                 " + ANSI_BLUE + "||");
+        System.out.println("||  " + ANSI_GREEN + "5. Exit" + ANSI_RESET + "                                                  " + ANSI_BLUE + "||");
+        System.out.println("===============================================================" + ANSI_RESET);
     }
 
     public void showExpenseMenu() {
-        System.out.println(ANSI_GREEN + "---------------- Expense Menu ---------------- \n" + ANSI_RESET +
-                ANSI_CYAN +"1. Add Expense " + ANSI_RESET +
-                "\n2. View Expenses");
+        System.out.println(ANSI_BLUE + """
+                ===============================================================
+                ||                     EXPENSE MENU                          ||
+                ===============================================================""");
+        System.out.println("||  " + ANSI_GREEN + "1. Add Expense" + ANSI_RESET + "                                           " + ANSI_BLUE + "||");
+        System.out.println("||  " + ANSI_GREEN + "2. View Expenses" + ANSI_RESET + "                                         " + ANSI_BLUE + "||");
+        System.out.println("===============================================================" + ANSI_RESET);
     }
 
     public void showIncomeMenu() {
-        System.out.println(ANSI_GREEN + "---------------- Income Menu ---------------- \n" + ANSI_RESET +
-                ANSI_CYAN + "1. Add Income\n" + ANSI_RESET +
-                "2. View Income\n");
+        System.out.println(ANSI_BLUE + """
+                ===============================================================
+                ||                      INCOME MENU                          ||
+                ===============================================================""");
+        System.out.println("||  " + ANSI_GREEN + "1. Add Income" + ANSI_RESET + "                                           " + ANSI_BLUE + "||");
+        System.out.println("||  " + ANSI_GREEN + "2. View Income" + ANSI_RESET + "                                         " + ANSI_BLUE + "||");
+        System.out.println("===============================================================" + ANSI_RESET);
     }
 
-    public void showEnterAmount() { System.out.print(ANSI_CYAN +"Enter amount: "+ ANSI_RESET); }
-
-    public void showEnterDescription() { System.out.print(ANSI_CYAN + "Enter description: " + ANSI_RESET); }
-
-    public void showRemoveTransaction() { System.out.print(ANSI_CYAN + "Enter Transaction ID to be removed: " + ANSI_RESET); }
-
     public void showTransactions(ArrayList<Transaction> transactions) {
-        System.out.println(ANSI_GREEN + "---------------- Transaction List ----------------" + ANSI_RESET);
+        System.out.println(ANSI_YELLOW + """
+                ===============================================================
+                ||                   TRANSACTION LIST                        ||
+                ===============================================================""");
+
         for (int i = 0; i < transactions.size(); i++) {
             Transaction transaction = transactions.get(i);
             String type = "";
             if (transaction.type()) type = "Expense";
             else type = "Income";
 
-            System.out.println((i + 1) + ". Amount: $" +
+            System.out.println(ANSI_WHITE + (i + 1) + ". Amount: $" +
                     transaction.amount() +
                     " | Description: " +
                     transaction.description() +
                     " | Type: " +
-                    type);
+                    type + ANSI_RESET);
         }
+
+        System.out.println(ANSI_YELLOW + "===============================================================" + ANSI_RESET);
     }
 
-    public void showEnterOption() { System.out.print("Enter your option: "); }
+    public void showEnterAmount() { System.out.print(ANSI_MAGENTA + ">>> Enter amount: " + ANSI_RESET); }
+
+    public void showEnterDescription() { System.out.print(ANSI_MAGENTA +">>> Enter description: " + ANSI_RESET); }
+
+    public void showRemoveTransaction() { System.out.print(ANSI_MAGENTA +">>> Enter Transaction ID to be removed: " + ANSI_RESET); }
+
+    public void showEnterOption() { System.out.print(ANSI_MAGENTA +">>> Enter option: " + ANSI_RESET); }
+
+    public void showInvalidOptionError() {
+        System.out.println(ANSI_RED +"!!!!!!!! The value you entered is an invalid type for this option. TRY AGAIN !!!!!!!!" + ANSI_RESET);
+        System.out.print(ANSI_MAGENTA +">>> " + ANSI_RESET);
+    }
 }
