f0163a5503b628e9eea683a9901c338ae5ae54b5
Nota30 <harenkeshaav11@gmail.com>
2025-10-27 22:00:29 -0400

    Add error handling for invalid types

--- src/main/java/edu/bsu/cs/Input.java
+++ src/main/java/edu/bsu/cs/Input.java
@@ -3,11 +3,26 @@ package edu.bsu.cs;
 import java.util.Scanner;
 
 public class Input {
+    private final Output output = new Output();
     private final Scanner input = new Scanner(System.in);
 
-    public int getOption() { return Integer.parseInt(input.nextLine()); }
+    public int getOption() {
+        try {
+            return Integer.parseInt(input.nextLine());
+        }  catch (NumberFormatException e) {
+            output.showInvalidOptionError();
+            return getOption();
+        }
+    }
 
     public String getDescription() { return input.nextLine(); }
 
-    public float getAmount() { return Float.parseFloat(input.nextLine()); }
+    public float getAmount() {
+        try {
+            return Float.parseFloat(input.nextLine());
+        }  catch (NumberFormatException e) {
+            output.showInvalidOptionError();
+            return getAmount();
+        }
+    }
 }
