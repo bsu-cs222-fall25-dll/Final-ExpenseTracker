0ab9ce5155012aca59920614fe50690d0e995a6a
Nota30 <harenkeshaav11@gmail.com>
2025-11-06 16:29:25 -0500

    Refactor main to use javafx

--- src/main/java/edu/bsu/cs/Main.java
+++ src/main/java/edu/bsu/cs/Main.java
@@ -1,36 +1,24 @@
 package edu.bsu.cs;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class Main extends Application {
+
     public static void main(String[] args) {
-        Store store = new Store();
-        Output output = new Output();
-        Controller controller = new Controller(store);
+            launch(args);
+    }
 
-        boolean continueLoop = true;
-        while (continueLoop) {
-            int option = controller.mainMenu();
-
-            switch (option) {
-                case 1:
-                    controller.expenseMenu();
-                    break;
-                case 2:
-                    controller.incomeMenu();
-                    break;
-                case 3:
-                    controller.removeTransaction();
-                    break;
-                case 4:
-                    controller.getAllTransactions();
-                    break;
-                case 5:
-                    continueLoop = false;
-                    break;
-                default:
-                    output.showInvalidOptionError();
-                    break;
-            }
-        }
-
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(Main.class.getResource("sample.fxml")));
+        primaryStage.setTitle("Expense Tracker");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
     }
 }
