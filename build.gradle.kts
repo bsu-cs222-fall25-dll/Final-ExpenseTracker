698babc0cea08ba394998a50eae6282d5de5195b
Nota30 <harenkeshaav11@gmail.com>
2025-11-06 14:38:03 -0500

    Add javafx dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,10 @@
 plugins {
     id("java")
+    id("application")
+    id("org.openjfx.javafxplugin") version "0.1.0"
 }
 
-group = "org.example"
+group = "edu.bsu.cs"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -18,4 +20,17 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+javafx {
+    version = "22"
+    modules("javafx.controls", "javafx.fxml")
+}
+
+application {
+    mainClass.set("edu.bsu.cs.UI")
+}
+
+tasks.named<JavaExec>("run") {
+    jvmArgs = listOf("--enable-native-access=javafx.graphics")
 }
