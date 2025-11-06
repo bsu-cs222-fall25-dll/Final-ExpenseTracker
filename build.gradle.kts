9ed91179f181dc3fc023ad94defbb0f973e0fd7a
cynkou <chuysluis@gmail.com>
2025-11-06 14:18:44 -0500

    Add dependencies in gradle build

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("io.github.palexdev:materialfx:11.17.0")
 }
 
 tasks.test {
