44338454440830dc4c712abcdeb084d3ac142008
Nota30 <harenkeshaav11@gmail.com>
2025-11-12 13:43:16 -0500

    Add reader class to parse csv files

---
+++ src/main/java/edu/bsu/cs/Reader.java
@@ -0,0 +1,51 @@
+package edu.bsu.cs;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Reader {
+    private final String fileName;
+
+    public Reader(String fileName) { this.fileName = fileName; }
+
+    public ArrayList<Transaction> loadAllTransactions() {
+        ArrayList<Transaction> transactions = new ArrayList<Transaction>();
+        File file = new File(fileName);
+
+        try {
+            Scanner scanner = new Scanner(file);
+            if (scanner.hasNextLine()) scanner.nextLine();
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] fields = line.split(",");
+                Transaction transaction = new Transaction(
+                        Float.parseFloat(fields[0]),
+                        Boolean.parseBoolean(fields[1]),
+                        fields[2]);
+                transactions.add(transaction);
+            }
+
+            return transactions;
+        } catch (IOException e) { return transactions; }
+    }
+
+    public void saveAllTransactions(ArrayList<Transaction> transactions) {
+        try {
+            FileWriter file = new FileWriter(fileName);
+            String header = "amount,type,description\n";
+            file.write(header);
+
+            for (Transaction transaction : transactions) {
+                String line = transaction.amount() + "," + transaction.type() + "," + transaction.description();
+                file.write(line + "\n");
+            }
+
+            file.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
