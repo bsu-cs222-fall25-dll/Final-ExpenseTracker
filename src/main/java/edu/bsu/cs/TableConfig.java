1c9a59695590fe7c168ac6d5d6d7f3573f0d6323
Nota30 <harenkeshaav11@gmail.com>
2025-11-13 02:24:19 -0500

    Add class table config to initialize the table

---
+++ src/main/java/edu/bsu/cs/TableConfig.java
@@ -0,0 +1,54 @@
+package edu.bsu.cs;
+
+import javafx.beans.property.SimpleFloatProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+
+public class TableConfig {
+
+    public void configureTable(
+            TableColumn<Transaction, Integer> idColumn,
+            TableColumn<Transaction, String> typeColumn,
+            TableColumn<Transaction, Float> amountColumn,
+            TableColumn<Transaction, String> descriptionColumn
+    ) {
+        configureIdColumn(idColumn);
+        configureTypeColumn(typeColumn);
+        configureAmountColumn(amountColumn);
+        configureDescriptionColumn(descriptionColumn);
+    }
+
+    private void configureIdColumn(TableColumn<Transaction, Integer> idColumn) {
+        idColumn.setCellFactory(column -> new TableCell<Transaction, Integer>() {
+            @Override
+            protected void updateItem(Integer item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty) setText(null);
+                else setText(String.valueOf(getIndex()));
+            }
+        });
+    }
+
+    private void configureTypeColumn(TableColumn<Transaction, String> typeColumn) {
+        typeColumn.setCellValueFactory(cellData -> {
+            boolean isExpense = cellData.getValue().type();
+            String typeStr = isExpense ? "Expense" : "Income";
+            return new SimpleStringProperty(typeStr);
+        });
+    }
+
+    private void configureAmountColumn(TableColumn<Transaction, Float> amountColumn) {
+        amountColumn.setCellValueFactory(cellData -> {
+            float amt = cellData.getValue().amount();
+            return new SimpleFloatProperty(amt).asObject();
+        });
+    }
+
+    private void configureDescriptionColumn(TableColumn<Transaction, String> descriptionColumn) {
+        descriptionColumn.setCellValueFactory(cellData -> {
+            String desc = cellData.getValue().description();
+            return new SimpleStringProperty(desc);
+        });
+    }
+}
