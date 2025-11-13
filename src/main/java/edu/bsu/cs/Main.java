9dbd83235e21c9ee8da2b2129bb12722ad6f255f
Nota30 <harenkeshaav11@gmail.com>
2025-11-13 14:04:19 -0500

    Add materialfx theme

--- src/main/java/edu/bsu/cs/Main.java
+++ src/main/java/edu/bsu/cs/Main.java
@@ -1,5 +1,8 @@
 package edu.bsu.cs;
 
+import io.github.palexdev.materialfx.theming.JavaFXThemes;
+import io.github.palexdev.materialfx.theming.MaterialFXStylesheets;
+import io.github.palexdev.materialfx.theming.UserAgentBuilder;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -14,6 +17,14 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        UserAgentBuilder.builder()
+                .themes(JavaFXThemes.CASPIAN)
+                .themes(MaterialFXStylesheets.forAssemble(true))
+                .setDeploy(true)
+                .setResolveAssets(true)
+                .build()
+                .setGlobal();
+
         Parent root = FXMLLoader.load(Objects.requireNonNull(Main.class.getResource("ExpenseTracker.fxml")));
         primaryStage.setTitle("Expense Tracker");
         primaryStage.setScene(new Scene(root));
